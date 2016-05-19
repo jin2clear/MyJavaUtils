2ce79378ad49ee25df48a96c49c4421f0ad3924d
jin2clear <jinxd2014@163.com>
2016-05-19 21:25:17 +0800

    Update Linux_command.sh

--- Linux_command.sh
+++ Linux_command.sh
@@ -2,4 +2,20 @@
          -r---->recursive 
          -n---->line number
          -w---->whole word
-2)  
+2)  awk '/MemTotal/{total=$2}/FreeTotal/{free=$2}END{print (total-free)/1024' /proc/memifo
+3)  euqals: free -m
+4) while true
+         do
+         #
+         #
+         done
+5)case $option in
+  1)
+  #
+  ;;
+  2)
+  #
+  ;;
+  esac
+6)ftp 192.168.1.1
+  sftp root@192.168.1.1 22
